9b5cc4ff39cfe0f7f2c9f62ba718f02721db216a
Justin Huynh <justinh800@gmail.com>
2017-04-30 21:09:53 -0700

    unused import

--- src/MinesUI.java
+++ src/MinesUI.java
@@ -8,7 +8,6 @@ import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.Insets;
 
 import java.awt.image.BufferedImage;
 import java.awt.event.KeyEvent;
